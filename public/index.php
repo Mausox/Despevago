e6d529d37626abdf4121a2ce22bc32be66497317
Cristobal Medrano Alvarado <cristobal.medrano@usach.cl>
2018-06-05 21:20:58 -0400

    Auto stash before merge of "develop" and "feature/prueba"

--- public/index.php
+++ public/index.php
@@ -1,7 +1,15 @@
 <!DOCTYPE html>
 <html>
 <head>
+<<<<<<< Updated upstream
 	<title>Hola mundo_Mauricio</title>
+=======
+<<<<<<< HEAD
+	<title>Hola mundo 3.0</title>
+=======
+	<title>Hola mundo_Mauricio</title>
+>>>>>>> prueba
+>>>>>>> Stashed changes
 </head>
 <body>
 
