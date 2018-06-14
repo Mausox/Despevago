50eec62ba38b9d9961cd44acf7061193317088c3
Chun-zen <chunzen1998@gmail.com>
2018-06-14 15:15:42 -0400

    Migration transfers created

---
+++ database/migrations/2018_06_14_191248_transfer_cars.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Aejnsn\Postgresify\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class TransferCars extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transfer_cars', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('vehicle_registration_number');
+            $table->integer('capacity');
+            $table->integer('category');
+            $table->string('company');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transfer_cars');
+    }
+}
