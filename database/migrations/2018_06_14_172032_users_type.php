d4ae41715e85dc401a3c57ac371ccf8f84bdb84d
Chun-zen <chunzen1998@gmail.com>
2018-06-14 15:11:42 -0400

    Migration users blueprint fix

--- database/migrations/2018_06_14_172032_users_type.php
+++ database/migrations/2018_06_14_172032_users_type.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Schema;
-use Illuminate\Database\Schema\Blueprint;
+use Aejnsn\Postgresify\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
 class UsersType extends Migration
