20b92c12ccd581c5ccd782f6aac3f5346c060605
Cristobal Medrano Alvarado <cristobal.medrano@usach.cl>
2018-06-14 15:52:14 -0400

    Migration seats created

---
+++ database/migrations/2018_06_14_194839_create_migration_seats_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+//use Illuminate\Database\Schema\Blueprint;
+use Aejnsn\Postgresify\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMigrationSeatsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('seats', function (Blueprint $table) {
+            $table->increments('seat_id');
+            $table->string('number');
+            $table->integer('status');
+            $table->money('price');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('seats');
+    }
+}
