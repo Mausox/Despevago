a092ef9c901c181320f43ed7b5f7b9f1f3723956
Gustavo <gustavo.hurtado@usach.cl>
2018-06-14 12:46:41 -0400

    Migration activities done

---
+++ database/migrations/2018_06_14_160454_create_table_activities.php
@@ -0,0 +1,37 @@
+<?php
+
+use Aejnsn\Postgresify\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+//use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+
+class CreateTableActivities extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('activities', function (Blueprint $table) {
+            $table->increments('activity_id');
+            $table->string('address');
+            $table->date('date');
+            $table->money('money');
+            $table->string('description');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('activities');
+    }
+}
