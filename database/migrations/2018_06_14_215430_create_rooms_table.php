da9372a72a9ed42c58e0ff47db55ccd0c9a1ee40
Gustavo <gustavo.hurtado@usach.cl>
2018-06-14 18:13:12 -0400

    Migrations from hotel

---
+++ database/migrations/2018_06_14_215430_create_rooms_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Aejnsn\Postgresify\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRoomsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('rooms', function (Blueprint $table) {
+            $table->increments('room_id');
+            $table->integer('capacity');
+            $table->money('adult_price');
+            $table->money('child_price');
+            $table->string('description');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('rooms');
+    }
+}
