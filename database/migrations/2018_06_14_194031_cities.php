a058b47a4328ee119a044e9384a954dcd3e0dd4c
Chun-zen <chunzen1998@gmail.com>
2018-06-14 15:43:55 -0400

    Migration ubications created

---
+++ database/migrations/2018_06_14_194031_cities.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Aejnsn\Postgresify\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class Cities extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cities', function (Blueprint $table) {
+            $table->increments('cities_id');
+            $table->string('name');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cities');
+    }
+}
