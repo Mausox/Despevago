f28bd327b90f7547f199528fad6f55bd8874fc57
Cristobal Medrano Alvarado <cristobal.medrano@usach.cl>
2018-06-15 15:02:57 -0400

    Main view

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,10 +1,10 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Hola mundo</title>
+    <title>Despevago</title>
 </head>
 <body>
-
+<a href="https://imgur.com/9Yw05Bc"><img src="https://i.imgur.com/9Yw05Bc.png" title="source: imgur.com" /></a>
 </body>
 </html>
 
