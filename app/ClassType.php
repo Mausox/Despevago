d8fb7c85cb7463120a4ba4e327f16b5711a8c646
Cristobal Medrano Alvarado <cristobal.medrano@usach.cl>
2018-06-21 21:48:19 -0400

    Relationship in Flights models finished

--- app/ClassType.php
+++ app/ClassType.php
@@ -6,9 +6,19 @@ use Illuminate\Database\Eloquent\Model;
 
 class ClassType extends Model
 {
+    /**
+     * @var string
+     */
     protected $primaryKey = 'class_type_id';
+
+    /**
+     * @var string
+     */
     protected $table = 'class_types';
 
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
     public function seats()
     {
         return $this->hasMany('App\Seat');
--- app/Passenger.php
+++ app/Passenger.php
@@ -6,9 +6,19 @@ use Illuminate\Database\Eloquent\Model;
 
 class Passenger extends Model
 {
+    /**
+     * @var string
+     */
     protected $primaryKey = 'passenger_id';
+
+    /**
+     * @var string
+     */
     protected $table = 'passengers';
 
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
     public function seat()
     {
         return $this->belongsTo('App/Seat');
