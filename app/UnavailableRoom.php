104560c779eeae087a7faf5a6c60f5bc63c641a4
Gustavo <gustavo.hurtado@usach.cl>
2018-06-21 20:53:20 -0400

    Relationshipincomplete unavailableroom

---
+++ app/UnavailableRoom.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class UnavailableRoom extends Model
+{
+    protected $primaryKey = 'unavailable_room';
+    protected $table = 'unavailable_rooms';
+
+    protected $date;
+
+    public function reservations ()
+    {
+        $this->belongsTo()
+    }
+
+}
