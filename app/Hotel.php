30ba694fa3553a4bc9e45fe7e3dd23fcbd5bcd19
Gustavo <gustavo.hurtado@usach.cl>
2018-06-23 16:44:08 -0400

    Hotel model done

---
+++ app/Hotel.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Hotel extends Model
+{
+    protected $primaryKey = 'hotel_id';
+    protected $table = 'hotels';
+
+    protected $name;
+    protected $email;
+    protected $score;
+    protected $description;
+
+    public function rooms()
+    {
+        $this->hasMany('App\Room');
+    }
+
+    public function hotelContacts()
+    {
+        $this->hasMany('App\HotelContact');
+    }
+
+    public function tranfers()
+    {
+        $this->hasMany('App\Transfer');
+    }
+
+}
