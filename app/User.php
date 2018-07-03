a828f519a399fc882cac8ac71b5f542d68a45425
Cristobal Medrano Alvarado <cristobal.medrano@usach.cl>
2018-07-03 00:05:19 -0400

    Added an user types validation and fillable fields

--- app/User.php
+++ app/User.php
@@ -10,15 +10,60 @@ class User extends Authenticatable
     use Notifiable;
 
     protected $primaryKey = 'user_id';
-
     protected $table = 'users';
+
+    public function reservations(){
+        return $this->hasMany('App/Reservation');
+    }
+
+    public function financial_information(){
+        return $this->hasOne('App/FinancialInformation');
+    }
+
+    public function user_history(){
+        return $this->belongsTo('App/UserHistory');
+    }
+
+    public function user_types(){
+        return $this->belongsToMany('App\UserType')->withTimestamps();
+    }
+
+    public function authorize_user_types($user_types){
+        if ($this->has_any_user_type($user_types)) {
+            return true;
+        }
+        abort(401, 'This action is unauthorized.');
+    }
+
+    public function has_any_user_type($user_types){
+        if (is_array($user_types)){
+            foreach ($user_types as $user_type){
+                if($this->has_user_type($user_type)){
+                    return true;
+                }
+            }
+        } else {
+            if($this->has_user_type($user_types)){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public function has_user_type($user_type){
+        if ($this->user_types()->where('name', $user_type)->first()){
+            return true;
+        }
+        return false;
+    }
+
     /**
      * The attributes that are mass assignable.
      *
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'email', 'password', 'first_name', 'last_name', 'telephone', 'address',
     ];
 
     /**
@@ -29,18 +74,4 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
-
-    public function reservations(){
-        return $this->hasMany('App/Reservation');
-    }
-    public function user_type(){
-        return $this->belongsTo('App/UserType');
-    }
-    public function financial_information(){
-        return $this->hasOne('App/FinancialInformation');
-    }
-    public function user_history(){
-        return $this->belongsTo('App/UserHistory');
-    }
-
 }
