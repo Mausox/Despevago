f91168b9618a3fb8e6d1aceb449577340a1230c7
Cristobal Medrano Alvarado <cristobal.medrano@usach.cl>
2018-07-02 23:31:31 -0400

    Intermediate table added user_types/users

---
+++ database/migrations/2018_07_02_232055_create_user_type_user_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUserTypeUserTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+
+    // This is an intermediate table -> user_types with users
+    public function up()
+    {
+        Schema::create('user_type_user', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_type_id')->unsigned();
+            $table->integer('user_id')->unsigned();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_type_user');
+    }
+}
