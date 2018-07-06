a508acb6e834eb050ef3cabd8f42907a41a75455
Cristobal Medrano Alvarado <cristobal.medrano@usach.cl>
2018-07-05 20:45:29 -0400

    Register view added

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -49,7 +49,7 @@
 
                         <!--Footer-->
                         <div class="modal-footer pt-3 mb-1">
-                            <p class="font-small grey-text d-flex justify-content-end">Not a member? <a href="#" class="blue-text ml-1"> Sign Up</a></p>
+                            <p class="font-small grey-text d-flex justify-content-end">Not a member? <a href="{{ route('register') }}" class="blue-text ml-1"> Sign Up</a></p>
                         </div>
                     </form>
                 </div>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,6 +1,67 @@
-@extends('layouts.app')
+@extends('despevago.auth')
+
+@section('title', 'Register')
 
 @section('content')
+
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8 form-elegant">
+            <div class="card">
+                <div class="card-body mx-4">
+                    <form method="POST" action="{{ route('register') }}">
+                        @csrf
+
+                        <div class="text-center">
+                            <h3 class="dark-grey-text mb-5"><strong>{{ __('Sign up') }}</strong></h3>
+                        </div>
+
+                        <div class="md-form">
+                            <label for="first_name">First name</label>
+                            <input id="first_name" type="text" class="form-control{{ $errors->has('first_name') ? ' is-invalid' : '' }}" name="first_name" value="{{ old('first_name') }}" required autofocus>                           
+                        </div>
+
+                        <div class="md-form">
+                            <label for="last_name">Last name</label>
+                            <input id="last_name" type="text" class="form-control{{ $errors->has('last_name') ? ' is-invalid' : '' }}" name="last_name" value="{{ old('last_name') }}">                           
+                        </div>
+
+                        <div class="md-form">
+                            <label for="email">Email</label>
+                            <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}">
+                        </div>
+
+                        <div class="md-form">
+                            <label for="password">Password</label>
+                            <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" value="{{ old('password') }}">
+                        </div>
+
+                        <div class="md-form">
+                            <label for="password_confirm">Confirm Password</label>
+                            <input id="password_confirm" type="password" class="form-control{{ $errors->has('password_confirm') ? ' is-invalid' : '' }}" name="password_confirm" value="{{ old('password_confirm') }}">
+                        </div>
+                        
+                        <div class="md-form">
+                            <label for="telephone">Telephone</label>
+                            <input id="telephone" type="text" class="form-control{{ $errors->has('telephone') ? ' is-invalid' : '' }}" name="telephone" value="{{ old('telephone') }}">                           
+                        </div>
+
+                        <div class="md-form pb-3">
+                            <label for="address">Address</label>
+                            <input id="address" type="text" class="form-control{{ $errors->has('address') ? ' is-invalid' : '' }}" name="address" value="{{ old('address') }}">                           
+                        </div>
+
+                        <div class="text-center mb-3">
+                            <button type="submit" class="btn blue-gradient btn-rounded btn-block">{{ __('Sign up') }}</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<!--
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
@@ -73,5 +134,5 @@
             </div>
         </div>
     </div>
-</div>
+</div> -->
 @endsection
