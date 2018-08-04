2bec35d7fd7f550515e17938cbe1b93dde5a70ae
Nameless <gustavo.hurtado@usach.cl>
2018-08-03 22:51:40 -0400

    Topbar fixed

--- resources/views/despevago/headers/headerMV.blade.php
+++ resources/views/despevago/headers/headerMV.blade.php
@@ -2,27 +2,28 @@
     <nav class="navbar navbar-expand navbar-dark blue darken-3">
         <div class="container">
             <ul class="navbar-nav mr-auto">
-                <li class="nav-item space {{ request()->is('/hotels') ? 'active' : ''}}">
-                    <a class="nav-link active" href="#"><i class="fas fa-hotel"></i> Accommodation</a>
+                <li class="nav-item space {{ request()->is('home') ? 'active' : ''}}">
+                    <a class="nav-link" href={{route('home')}}><i class="fas fa-home"></i> Home</a>
                 </li>
-                <li class="nav-item space {{ request()->is('/flights') ? 'active' : ''}}">
+                <li class="nav-item space {{ request()->is('hotels') ? 'active' : ''}}">
+                    <a class="nav-link active" href="{{url('/hotels')}}"><i class="fas fa-hotel"></i> Accommodation</a>
+                </li>
+                <li class="nav-item space {{ request()->is('flights') ? 'active' : ''}}">
                     <a class="nav-link" href="#"><i class="fas fa-plane"></i> Flights</a>
                 </li>
-                <li class="nav-item space {{ request()->is('/') ? 'active' : ''}}">
+                <li class="nav-item space {{ request()->is('packages') ? 'active' : ''}}">
                     <a class="nav-link" href="#"><i class="fas fa-suitcase"></i> Packages</a>
                 </li>
-                <li class="nav-item space {{ request()->is('/activities') ? 'active' : ''}}">
+                <li class="nav-item space {{ request()->is('activities') ? 'active' : ''}}">
                     <a class="nav-link" href="#"><i class="fas fa-map-marked-alt"></i> Activities</a>
                 </li>
-                <li class="nav-item space {{ request()->is('/cars') ? 'active' : ''}}">
+                <li class="nav-item space {{ request()->is('cars') ? 'active' : ''}}">
                     <a class="nav-link" href="#"><i class="fas fa-car"></i> Cars</a>
                 </li>
-                <li class="nav-item space {{ request()->is('/transfers') ? 'active' : ''}}">
+                <li class="nav-item space {{ request()->is('transfers') ? 'active' : ''}}">
                     <a class="nav-link" href="#"><i class="fas fa-shuttle-van"></i> Transfers</a>
                 </li>
-                <li class="nav-item space {{ request()->is('/rentals') ? 'active' : ''}}">
-                    <a class="nav-link" href="#"><i class="fas fa-street-view"></i> Rentals</a>
-                </li>
+
             </ul>
         </div>
     </nav>
