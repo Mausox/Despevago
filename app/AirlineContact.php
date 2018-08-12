fcaaed670d15a8a50f5746490c02d1ce84754de4
Cristobal Medrano Alvarado <cristobal.medrano@usach.cl>
2018-08-12 03:33:46 -0300

    CRUD for AirlineContact Added
    Added a foreign key

--- app/AirlineContact.php
+++ app/AirlineContact.php
@@ -6,6 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class AirlineContact extends Model
 {
+     /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'telephone', 'airline_id',
+    ];
+
     public function airline()
     {
         return $this->belongsTo(Airline::class);
