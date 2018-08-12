68c8864cca14282f47dd4e1b93aaf929803af848
Cristobal Medrano Alvarado <cristobal.medrano@usach.cl>
2018-08-12 03:17:23 -0300

    CRUD for ClassType Added

--- app/ClassType.php
+++ app/ClassType.php
@@ -6,6 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class ClassType extends Model
 {
+     /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name',
+    ];
+
     public function seats()
     {
         return $this->hasMany(Seat::class);
