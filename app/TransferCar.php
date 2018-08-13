5cefcddefad36fa7ad777fbe56e29ce10c030cbd
Chun-zen <chun-zen.yu@usach.cl>
2018-08-13 03:51:49 -0300

    [CRUD] Transfer car CRUD added
    Created CRUD for transfer car

--- app/TransferCar.php
+++ app/TransferCar.php
@@ -6,6 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class TransferCar extends Model
 {
+    protected $fillable = [
+        'vehicle_registration_number', 'capacity', 'category', 'company', 'transfer_id',
+        ];
     public function transfer(){
         return $this->belongsTo(Transfer::class);
     }
