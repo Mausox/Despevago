17a9b0e2dcb81950b8a67d48c7f35c0e5da7abbe
Chun-zen <chun-zen.yu@usach.cl>
2018-08-13 03:42:59 -0300

    [CRUD] Transfer CRUD added
    created CRUD for transfer

---
+++ app/Http/Controllers/TransferController.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Transfer;
+
+class TransferController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Transfer::all();
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $transfer = Transfer::create($request->all());
+        return $transfer;
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        return Transfer::find($id);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        Transfer::find($id)->update($request->all());
+        return "The transfer ID: {$id} was updated!";
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        Transfer::destroy($id);
+        return "The transfer ID: {$id} was removed!";
+    }
+}
--- app/Transfer.php
+++ app/Transfer.php
@@ -5,6 +5,10 @@ namespace App;
 use Illuminate\Database\Eloquent\Model;
 
 class Transfer extends Model
+{
+    protected $fillable = [
+        'start_date', 'end_date', 'number_people', 'price', 'hotel_id', 'airport_id',
+    ];
 
     public function reservations(){
         return $this->belongsToMany(Reservation::class);
