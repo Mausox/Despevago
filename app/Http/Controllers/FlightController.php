83209e5e40fff3a596e5ba3288ffa8ceb606d7bf
Cristobal Medrano Alvarado <cristobal.medrano@usach.cl>
2018-08-17 14:48:59 -0300

    [CRUD] Airport and Flight added

---
+++ app/Http/Controllers/FlightController.php
@@ -0,0 +1,87 @@
+<?php  
+  
+namespace App\Http\Controllers;  
+  
+use Illuminate\Http\Request;  
+use App\Flight;  
+  
+class FlightController extends Controller  
+{  
+    /**  
+     * Display a listing of the resource.  
+     *  
+     * @return \Illuminate\Http\Response  
+     */  
+    public function index()  
+    {  
+        return Flight::all();  
+    }  
+  
+    /**  
+     * Show the form for creating a new resource.  
+     *  
+     * @return \Illuminate\Http\Response  
+     */  
+    public function create()  
+    {  
+        //  
+    }  
+  
+    /**  
+     * Store a newly created resource in storage.  
+     *  
+     * @param  \Illuminate\Http\Request  $request  
+     * @return \Illuminate\Http\Response  
+     */  
+    public function store(Request $request)  
+    {  
+        return Flight::create($request->all());  
+    }  
+  
+    /**  
+     * Display the specified resource.  
+     *  
+     * @param  int  $id  
+     * @return \Illuminate\Http\Response  
+     */  
+    public function show($id)  
+    {  
+        return Flight::find($id);   
+    }  
+  
+    /**  
+     * Show the form for editing the specified resource.  
+     *  
+     * @param  int  $id  
+     * @return \Illuminate\Http\Response  
+     */  
+    public function edit($id)  
+    {  
+        //  
+    }  
+  
+    /**  
+     * Update the specified resource in storage.  
+     *  
+     * @param  \Illuminate\Http\Request  $request  
+     * @param  int  $id  
+     * @return \Illuminate\Http\Response  
+     */  
+    public function update(Request $request, $id)  
+    {  
+        Flight::find($id)->update($request->all());   
+        return "Flight {$id} updated!"; 
+    }  
+  
+    /**  
+     * Remove the specified resource from storage.  
+     *  
+     * @param  int  $id  
+     * @return \Illuminate\Http\Response  
+     */  
+    public function destroy($id)  
+    {  
+        Flight::destroy($id); 
+        return "Flight {$id} was deleted"; 
+    }  
+}
